9fb0e3300b8a9a379e8d2a615682739deb98b27e
FADVAN8 <galiwango.fahad@gmail.com>
2020-11-10 08:46:01 +0300

    removed retrofit dependencies and its services

--- src/main/java/com/wanfadger/RetrofitgwtRpctodo/server/actionhandlers/TodoActionHandler.java
+++ src/main/java/com/wanfadger/RetrofitgwtRpctodo/server/actionhandlers/TodoActionHandler.java
@@ -1,7 +1,5 @@
 package com.wanfadger.RetrofitgwtRpctodo.server.actionhandlers;
 
-import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -13,11 +11,8 @@ import com.gwtplatform.dispatch.shared.ActionException;
 import com.wanfadger.RetrofitgwtRpctodo.shared.actionresult.TodoAction;
 import com.wanfadger.RetrofitgwtRpctodo.shared.actionresult.TodoResult;
 import com.wanfadger.RetrofitgwtRpctodo.shared.models.Todo;
-import com.wanfadger.RetrofitgwtRpctodo.shared.retrofit.RetrofitServiceGenerator;
-import com.wanfadger.RetrofitgwtRpctodo.shared.retrofit.services.TodoService;
 import com.wanfadger.RetrofitgwtRpctodo.shared.utils.TodoActionCommands;
 
-import retrofit2.Response;
 
 public class TodoActionHandler implements ActionHandler<TodoAction , TodoResult> {
 
@@ -29,41 +24,41 @@ public class TodoActionHandler implements ActionHandler<TodoAction , TodoResult>
 	
 	@Override
 	public TodoResult execute(TodoAction action, ExecutionContext context) throws ActionException {
-		TodoService service = RetrofitServiceGenerator.createService(TodoService.class);
+		//TodoService service = RetrofitServiceGenerator.createService(TodoService.class);
 		
 		if(action.getCommand().equals(TodoActionCommands.FETCH_TODOS)) {
 			
 		
-			List<Todo> todos = new ArrayList<Todo>();
-			try {
-				Response<List<Todo>> todoResponse = service.fetchTodos().execute();
-			    todos = todoResponse.body();
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-				System.out.print("ERROR "+e.getMessage());
-			}
+//			List<Todo> todos = new ArrayList<Todo>();
+//			try {
+//				Response<List<Todo>> todoResponse = service.fetchTodos().execute();
+//			    todos = todoResponse.body();
+//			} catch (IOException e) {
+//				// TODO Auto-generated catch block
+//				e.printStackTrace();
+//				System.out.print("ERROR "+e.getMessage());
+//			}
 //			
-//			List<Todo> todos  = Arrays.asList(
-//					new Todo("1" , "todo 1") , 
-//					new Todo("2" , "todo 2") ,
-//					new Todo("3" , "todo 3") ,
-//					new Todo("4" , "todo 4") ,
-//					new Todo("5" , "todo 5"));
-//			
+			List<Todo> todos  = Arrays.asList(
+					new Todo("1" , "todo 1") , 
+					new Todo("2" , "todo 2") ,
+					new Todo("3" , "todo 3") ,
+					new Todo("4" , "todo 4") ,
+					new Todo("5" , "todo 5"));
+			
 			
 			return new TodoResult(todos);
 			
 		}else if(action.getCommand().equals(TodoActionCommands.SAVE_TODO)) {
 			
-			Todo todo = null;
-			try {
-				Response<Todo> todoResponse = service.saveTodo(action.getTodo()).execute();
-			     todo = todoResponse.body();
-			} catch (IOException e) {
-				e.printStackTrace();
-				System.out.print("Error "+e.getMessage());
-			}
+			Todo todo = new Todo("5" , "todo 5");
+//			try {
+//				Response<Todo> todoResponse = service.saveTodo(action.getTodo()).execute();
+//			     todo = todoResponse.body();
+//			} catch (IOException e) {
+//				e.printStackTrace();
+//				System.out.print("Error "+e.getMessage());
+//			}
 			
 		  return new TodoResult(todo);	
 		} else {
